8a426c61fcd579b310503d0646fe02eaa7c11a49
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2022-01-24 12:14:00 +0530

    added changes

--- src/main/java/com/lambdatest/tunnel/TestDaemonThread1.java
+++ src/main/java/com/lambdatest/tunnel/TestDaemonThread1.java
@@ -4,17 +4,20 @@ import com.lambdatest.httpserver.HttpServer;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import com.lambdatest.Utils;
 
 public class TestDaemonThread1 extends Thread {
 
     public Integer port = null;
+    Utils utils = new Utils();
 
     public void run() {
         if (Thread.currentThread().isDaemon()) {//checking for daemon thread
             HttpServer httpServer = new HttpServer();
             try {
                 port = HttpServer.findAvailablePort();
-                System.out.println(port);
+                utils.logger(port.toString());
+//                System.out.println(port);
                 int[] myIntArray = {port};
                 httpServer.main(myIntArray);
             } catch (IOException e) {
